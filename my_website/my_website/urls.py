18ec92e451e481b9ba902501ad017250819b970e
Rex Mitchell <nogardjmj@gmail.com>
2020-05-17 17:13:42 -0500

    Reorganizing Project Apps
    Contact app successfully created.
    Changing where static files are stored.

--- my_website/my_website/urls.py
+++ my_website/my_website/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('', include('projects.urls')),
     path('contact/', include('contact.urls')),
     path('projects/', TemplateView.as_view(template_name='projects/home.html')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # Adds an extra layer of security for deployment
 # if settings.DEBUG:
